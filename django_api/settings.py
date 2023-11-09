04ba3581dfc25aa6e9b42bf4a8b8687785ca6673
amrylnikov <amrylnikov@gmail.com>
2023-11-09 15:29:09 +0300

    updated database

--- django_api/settings.py
+++ django_api/settings.py
@@ -2,7 +2,7 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,6 +60,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_api.wsgi.application'
 
 DATABASES = {
+    # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
