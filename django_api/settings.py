27897d4ad6a28e2e3f940656c8f65c7150cbd8f7
amrylnikov <amrylnikov@gmail.com>
2023-11-09 15:41:19 +0300

    changed connection to database

--- django_api/settings.py
+++ django_api/settings.py
@@ -60,11 +60,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_api.wsgi.application'
 
 DATABASES = {
-    # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),    
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
