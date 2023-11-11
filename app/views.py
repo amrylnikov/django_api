7767fd7b23da188a0b3d1c8091573e47efba07b2
amrylnikov <amrylnikov@gmail.com>
2023-11-11 11:12:05 +0300

    added layout api

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
+import random
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -8,3 +11,14 @@ from .serializers import ItemSerializer
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+class RandomNumberView(APIView):
+    def get(self, request, number, *args, **kwargs):
+        if number % 2 != 0:
+            return Response({"error": "Number should be even"}, status=status.HTTP_400_BAD_REQUEST)
+
+        half_number = number // 2
+        cards = list(range(1, half_number + 1)) * 2
+        random.shuffle(cards)
+
+        return Response({"cards": cards}, status=status.HTTP_200_OK)
